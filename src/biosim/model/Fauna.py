0a181de930e48ec1fa34c9e0dbbeb7a2484de0ff
Tonje Martine Lorgen Kirkholt <tonje.martine.lorgen.kirkholt@nmbu.no>
2023-01-11 09:08:44 +0100

    Changing placement of some of the documentation: cleaning it up.

--- src/biosim/model/Fauna.py
+++ src/biosim/model/Fauna.py
@@ -11,7 +11,9 @@ from biosim.exception import NotToBeUsedException
 
 class Fauna:
     """
-    A super class used to represent each species on Rossumøya: herbivores and carnivores
+    Super class used to represent each species on Rossumøya: Herbivores and Carnivores
+
+    Subclasses: Herbivore and Carnivore
     ...
 
     Attributes
@@ -26,24 +28,45 @@ class Fauna:
     A class method that changes the default values for the aninmal parameters. The user can change
     any of the values, and as many as preferred, before the simulation is started.
 
-    fitness(self):
+    weight_of_baby(mean, sd)
+    Method that calculates the weight of the newborn from an animal of type Herbivore or Carnivore.
+    Parameters are defined beforehand, and changes depending on the Fauna (Herbivore or Carnivore).
+
+    calculate_fitness(self):
     Method that changes the fitness for each animal, which describes the overall condition of
     the animal. The value changes each annual cycle, calculated based on age and weight using
     the formula (3) and equation values are given by formula (4).
     # TODO Add the formula in the description, or refer to it in another way
 
-    procreate()
+    new_animal(self, param, w_baby)
+    Method that is used to create a new animal of type Herbivore or Carnivore.
 
-    feed()
+    _change_weight(self, by_amount)
+    Private method that calculates the new weight of an animal. The current weight either decreseas
+    or increases, but if a decrease results in a negative value the weight is set to 0.
 
-    migrate()
+    procreate(self, N: int)
+    Method that decides which animals are producing an offspring. The following conditions are set:
+    a) An animal can only have offspring if its weight < zeta (w_birth + sigma_birth)
+    b) For each animal there is a probability for producing offspring, which is
+     min(1, (gamma * fitness * N)) where N is the number of animals of the same species in the cell at
+     the start of the breeding season. The gamma value is set before the simulation, either by user
+     or by default, and the fitness is calculated by the method calculate_fitness(self).
+     c) Each animal can give birth to at most one offspring per year.
+     d) At birth, the parent loses xi times the actual birthweight of the baby.
+     e) If the parent would lose more than their own weight, no baby is born and the weight of the
+     parent remains unchanged.
 
-    aging()
+     get_older(self)
+     Method that adds one year to the animal after an annual cycle.
 
-    weightloss()
+     lose_weight(self)
+     Method that uses the _change_weight method to decrease the weight of an animal.
 
-    death()
-
+     maybe_die(self)
+     A boolean method that checks if an animal dies or not. Two conditions decides: if the animals
+     weight is equal to zero, death definitely follows. Then there is a probability condition, set
+     by the following equation: omega(1 - fitness).
     """
 
 
@@ -51,6 +74,12 @@ class Fauna:
 
     @classmethod
     def set_animal_parameters(cls, params: {}):
+        """
+        Set the parameters of an animal, either by default or by the input of a user
+        Parameters
+        ----------
+        params: {}
+        """
         for key, value in params.items():
             try:
                 if cls._params.key is not None:
@@ -62,6 +91,14 @@ class Fauna:
 
     @staticmethod
     def weight_of_baby(mean, sd):
+        """
+        Calculate the weight of a newborn animal
+
+        Parameters
+        ----------
+        mean: int
+        sd: int
+        """
         return random.lognormvariate(mean, sd)
 
     def __init__(self, age=0, weight=0):
@@ -71,7 +108,9 @@ class Fauna:
 
     @property
     def calculate_fitness(self):
-        """Calculate fitness."""
+        """
+        Calculate and return the fitness of an animal
+        """
         if self._weight <= 0:
             return 0
         q_plus = 1 / (
@@ -83,13 +122,37 @@ class Fauna:
     # possible methods:
 
     def new_animal(self, param, w_baby):
+        """
+        Create a new animal
+
+        Parameters
+        ----------
+        param: {}
+        w_baby: int
+
+        """
         raise NotToBeUsedException
 
     def _change_weight(self, by_amount):
+        """
+        Change the weight of an animal based on set conditions
+
+        Parameters
+        ----------
+        by_amount: int
+        """
         self._weight = max(self._weight + by_amount, 0)
 
     # 1
     def procreate(self, N: int):
+        """
+        Procreation of new animals. An offspring is produced based on five different conditions.
+
+        Parameters
+        ----------
+        N: int
+
+        """
         # a:
         if self._weight < self._params.zeta * (self._params.w_birth + self._params.sigma_birth):
             return None
@@ -106,14 +169,24 @@ class Fauna:
 
     # 4.
     def get_older(self):
+        """
+        Adds one year to an animals age
+        """
         self._age += 1
 
     # 5.
     def lose_weight(self):
+        """
+        Decreases the weight of an animal
+
+        """
         self._change_weight(-self._params.eta * self._params.omega)
 
     # 6.
     def maybe_die(self) -> bool:
+        """
+        Check if an animal is likely to die or not, returning either true or false
+        """
         if self._weight <= 0:
             return True
         else:
