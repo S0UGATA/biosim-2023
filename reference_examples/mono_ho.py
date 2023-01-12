e839ed3fa0a49acb2f52c598746d3ee85a31170a
Tonje Martine Lorgen Kirkholt <tonje.martine.lorgen.kirkholt@nmbu.no>
2023-01-12 10:13:27 +0100

    Startet adding documentation to UnitArea.py, changed the number of seeds in mono_ho.py

--- reference_examples/mono_ho.py
+++ reference_examples/mono_ho.py
@@ -23,4 +23,4 @@ ini_herbs = [{'loc': (2, 2),
 for seed in range(100, 103):
     sim = BioSim(geogr, ini_herbs, seed=seed,
                  img_dir='results', img_base=f'mono_ho_{seed:05d}', img_years=300)
-    sim.simulate(3001)
+    sim.simulate(301)
