ad5bbd3f06d274f5ef5f7fcd537ea9589b27b333
Tonje Martine Lorgen Kirkholt <tonje.martine.lorgen.kirkholt@nmbu.no>
2023-01-13 09:43:48 +0100

    Added documentation in UnitArea.py. Still need to go over, but most important information is now present in the file.

--- src/biosim/model/UnitArea.py
+++ src/biosim/model/UnitArea.py
@@ -27,6 +27,16 @@ class UnitArea:
 
     @staticmethod
     def _find_geo(geo) -> Geography:
+        """
+        Initializing and creating the map of the island by defining each UnitArea with the right
+        geo value: H for Highland, L for Lowland, W for water, and D for Desert.
+
+        Parameters
+        ----------
+        geo
+        Specifying the type of the UnitArea.
+
+        """
         match geo:
             case "H":
                 return Highland()
@@ -40,18 +50,58 @@ class UnitArea:
                 raise ValueError(f"Geography {geo} is not a valid value.")
 
     def total_pop(self):
+        """
+        Returning the total population of animals, including both Herbivores and Carnivores
+        """
         return len(self._herbs) + len(self._carns)
 
     def add_herb(self, herbivore: Herbivore):
+        """
+        Adding a Herbivore to the list of Herbivores, used in the initialization of the island
+        and first population of the island (see populate_island(self, population: [{}]) in file
+        Rossumoya.py)
+
+        Parameters
+        ----------
+        herbivore
+        A single herbivore
+        """
         self._herbs.append(herbivore)
 
     def add_herbs(self, herbivores: [Herbivore]):
+        """
+        Adding newborn Herbivores to the list of Herbivores
+        Parameters
+        ----------
+        herbivores
+
+        Returns
+        -------
+
+        """
         self._herbs.extend(herbivores)
 
     def add_carn(self, carnivore):
+        """
+        Adding a Carnivore to the list of Carnivores, used in the initialization of the island
+        and first population of the island (see populate_island(self, population: [{}]) in file
+        Rossumoya.py)
+
+        Parameters
+        ----------
+        carnivore
+        """
         self._carns.append(carnivore)
 
     def add_carns(self, carnivores: [Carnivore]):
+        """
+        Adding newborn Carnivores to the list of Carnivores.
+
+        Parameters
+        ----------
+        carnivores
+        List of newborn Carnivores
+        """
         self._carns.extend(carnivores)
 
     @property
@@ -63,6 +113,13 @@ class UnitArea:
         return self._geo
 
     def make_babies(self):
+        """
+        Create new Herbivores as a part of the annual cycle. Uses the method
+        procreate(self, number_of_animals: int) to verify necessary conditions that must be met
+        to produce new offspring. The offspring is added to a list containing the newborn from
+        the current cycle. This list is used in the method add_herbs(self, herbivores: [Herbivore])
+        at the end of the current cycle.
+        """
         no_herbs = len(self.herbs)
         babies = []
         for herb in self.herbs:
@@ -73,6 +130,14 @@ class UnitArea:
             self.add_herbs(babies)
 
     def eat(self):
+        """
+        Decides which animals get to eat. The amount of fodder is set by the parameter f_max, value
+        is initialized in the creation of the island and start of simulation. The list of animals
+        is randomly shuffled to ensure that the animal that gets to eat is chosen by random.
+        As long as there is food, animals get to eat. If the value of the remaining fodder is
+        equal to 0, cycle breaks and no more animals get to eat.
+
+        """
         remaining_fodder = self.geo.params.f_max
         herb_indices = [*range(len(self.herbs))]
         random.shuffle(herb_indices)
@@ -81,15 +146,31 @@ class UnitArea:
                 break
             remaining_fodder = self.herbs[index].feed_and_gain_weight(remaining_fodder)
 
+    def wander_away(self, cells):
+        """
+        Migration of animals to neighbouring UnitAreas as step 3 in the annual cycle of the island.
+
+        """
+        # TODO migration
+        pass
+
     def grow_old(self):
+        """
+        Adds one year to the current year of an animal as step 4 in the annual cycle of the island.
+        Uses method get_older(self) in Fauna.py.
+        """
         [herb.get_older() for herb in self.herbs]
 
     def get_thin(self):
+        """
+        Decreases the weight of an animal as step 5 in the annual cycle of the island.
+        Uses the method lose_weight(self) in Fauna.py.
+        """
         [herb.lose_weight() for herb in self.herbs]
 
     def maybe_die(self):
+        """
+        Checks if an animal is likely to die or not as step 6 in the annual cycle of the island.
+        Uses the method maybe_die(self) in Fauna.py
+        """
         self._herbs = [herb for herb in self.herbs if not herb.maybe_die()]
-
-    def wander_away(self, cells):
-        # TODO migration
-        pass
