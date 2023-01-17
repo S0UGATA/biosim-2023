2c074297d8e44a34accf25a496b2e2e0a0ed7fbe
Tonje Martine Lorgen Kirkholt <tonje.martine.lorgen.kirkholt@nmbu.no>
2023-01-17 10:05:25 +0100

    Adding test files for the main files in src/biosim/model -> testing fauna.py, geography.py, paramteres.py, rossumoya.py, and unit_area.py.

---
+++ tests/test_fauna.py
@@ -0,0 +1,10 @@
+# The material in this file is licensed under the BSD 3-clause license
+# https://opensource.org/licenses/BSD-3-Clause
+# (C) Copyright 2023 Tonje, Sougata / NMBU
+
+"""
+Test set for Fauna super class interface, in addition to child classes Herbivore and Carnivore
+interface.
+"""
+
+import pytest
---
+++ tests/test_rossumoya.py
@@ -0,0 +1,9 @@
+# The material in this file is licensed under the BSD 3-clause license
+# https://opensource.org/licenses/BSD-3-Clause
+# (C) Copyright 2023 Tonje, Sougata / NMBU
+
+"""
+Test set for Rossumøya class interface.
+"""
+
+import pytest
