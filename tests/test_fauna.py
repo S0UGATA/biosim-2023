6f21561a0f703e2b9ec915be7df1e68accfe15c1
Tonje Martine Lorgen Kirkholt <tonje.martine.lorgen.kirkholt@nmbu.no>
2023-01-17 11:03:10 +0100

    More testst added, specifically eating and weight gain for Herbivores and Carnivores.

--- tests/test_fauna.py
+++ tests/test_fauna.py
@@ -11,14 +11,20 @@ import pytest
 from biosim.model.fauna import Herbivore, Carnivore
 from biosim.model.parameters import FaunaParam
 
-herb = Herbivore()
-carn = Carnivore()
+""" Instances were initialized weight and age is zero, in addition to input of age and weight"""
 
 
 def reset_animal_params():
     """ Reset the animal parameters before running the tests."""
     yield
     Herbivore.set_animal_parameters(Herbivore.default_params)
+    Carnivore.set_animal_parameters(Herbivore.default_params)
+
+
+herb = Herbivore()
+carn = Carnivore()
+herb_set = Herbivore(5, 20)
+carn_set = Carnivore(6, 24)
 
 
 def test_age_carn_herb():
@@ -26,8 +32,12 @@ def test_age_carn_herb():
     assert herb.age == 0
     assert carn.age == 0
 
+    assert herb_set.age == 5
+    assert carn_set.age == 6
+
 
 def test_get_older_carn_herb():
+    """ Animal growing 1 year older.  """
     herb.get_older()
     carn.get_older()
     assert herb.age != 0
@@ -36,3 +46,29 @@ def test_get_older_carn_herb():
     herb.get_older()
     herb.get_older()
     assert herb.age == 3
+
+    herb_set.get_older()
+    carn_set.get_older()
+    assert herb_set.age == 6
+    assert carn_set.age == 7
+
+
+def test_eat_and_gain_weight_herb():
+    """Herbivore eating fodder and gaining weight. Weight gain is Fodder*beta, where beta = 0.9"""
+    fodder = 10
+    herb.feed_and_gain_weight(fodder)
+    assert herb.weight == pytest.approx(9)
+
+    herb_set.feed_and_gain_weight(fodder)
+    assert herb_set.weight == pytest.approx(29)
+
+def test_eat_and_gain_weight_carn():
+    """Carnivore eating and gaining weight. Weight gain is Fodder*beta, where beta = 0.75"""
+    herbs = [herb, herb_set]
+    carn_set.feed_on_herbivores_and_gain_weight(herbs)
+    assert carn_set.weight != 24
+    assert carn_set.weight > 24
+
+
+
+
