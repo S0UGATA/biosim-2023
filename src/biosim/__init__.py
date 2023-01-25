45d7c6e38ac28ed99ce8e173174056b3b43bd546
Sougata Bhattacharya <sougata.bhattacharya@nmbu.no>
2023-01-25 10:00:31 +0100

    v1.0

--- src/biosim/__init__.py
+++ src/biosim/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3"
+__version__ = "1.0"
 
 __author__ = "Tonje Martine Lorgen Kirkholt", "Sougata Bhattacharya"
 __email__ = "tonje.martine.lorgen.kirkholt@nmbu.no", "sougata.bhattacharya@nmbu.no"
